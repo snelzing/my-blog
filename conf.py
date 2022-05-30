e320b9d4513d6b3f4208bf50d2d06f846cfadedb
snelzing <39928924+snelzing@users.noreply.github.com>
2022-05-30 02:27:01 -0400

    Nikola auto commit.
    Nikola version: 8.2.2

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Scooter Girl"  # (translatable)
-BLOG_TITLE = "Elzinga.tech"  # (translatable)
+BLOG_TITLE = "Blog of The Scooter Girl"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://elzinga.tech/"
